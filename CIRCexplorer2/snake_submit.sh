6111adab2a1a7a4699d3252a6537f3310df8c7de
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-04-15 10:13:46 -0400

    Update snake_submit.sh

--- CIRCexplorer2/snake_submit.sh
+++ CIRCexplorer2/snake_submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=snake_circ
+#SBATCH --job-name=snake_circexplorer
 #SBATCH --mem=3000M
 #SBATCH -c 1
-#SBATCH -t 3:00:00
+#SBATCH -t 72:00:00
 
 source /cluster/home/amammoli/.bashrc
 module load python3
@@ -11,4 +11,4 @@ module load python3
 
 
 
-output_dir="/cluster/projects/bhklab/Data/ncRNA_detect/circRNA/CIRCexplorer2/test"  snakemake -s /cluster/projects/bhklab/Data/ncRNA_detect/circRNA/CIRCexplorer2/test/Snakefile --latency-wait 100 -j 200 --cluster 'sbatch -t {params.runtime} --mem={resources.mem_mb} -c {threads}'
+output_dir="/cluster/projects/bhklab/Data/ncRNA_detect/circRNA/CIRCexplorer2/test"  snakemake -s /cluster/projects/bhklab/Data/ncRNA_detect/circRNA/CIRCexplorer2/test/Snakefile --latency-wait 100 -j 200 --cluster 'sbatch -t {params.runtime} --mem={resources.mem_mb} -c {threads} -p {params.partition}'
