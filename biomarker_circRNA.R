14a001aad34055223e477bcca7aa5deb5c83abc4
imanthonny <34114220+imanthonny@users.noreply.github.com>
2020-05-29 22:08:23 -0400

    Update biomarker_circRNA.R

--- biomarker_circRNA.R
+++ biomarker_circRNA.R
@@ -6,6 +6,8 @@ library(survcomp)
 library(DESeq2)
 library(dplyr)
 library(stringr)
+library(gimme)
+#library(plyr)
 
 options(stringsAsFactors = F)
 
@@ -20,6 +22,19 @@ matchToIDTable <- function(ids,tbl, column, returnColumn="unique.cellid") {
   })
 }
 
+expand.grid.unique <- function(x, y, include.equals=FALSE)
+{
+  x <- unique(x)
+  y <- unique(y)
+  g <- function(i)
+  {
+    z <- setdiff(y, x[seq_len(i-include.equals)])
+    
+    if(length(z)) cbind(x[i], z, deparse.level=0)
+  }
+  do.call(rbind, lapply(seq_along(x), g))
+}
+
 #read in current cell annotations
 cell_all <- read.csv(file = "~/Desktop/Projects/BHKLAB/ncRNA/rnaseq_meta/cell_annotation_all.csv", na.strings=c("", " ", "NA"))
 #gcsi metadata
@@ -28,7 +43,7 @@ gcsi$cellid <- matchToIDTable(ids=gcsi$Cell_line , tbl = cell_all, column = "GNE
 rownames(gcsi) <- gcsi$alias
 
 ##########################
-#####DESeq2 for CIRI2#####
+#####CIRI2 Exp Matrix#####
 ##########################
 
 #create gene count matrix (# of exonic junction reads >=2 per gene)
@@ -70,8 +85,12 @@ summarizeCIRIMatrix <- function(dir_path){
 } 
 
 gcsi_ciri_matrix <- summarizeCIRIMatrix(dir_path = "/Users/anthmam/Desktop/Projects/BHKLAB/ncRNA/results/CIRI2/gCSI/result")
+gcsi_ciri_matrix_norm <- log2(gcsi_ciri_matrix + 1) #dont do this for deseq2, leave matrix untouched.
+
+##########################
+######CIRI2 DEseq2########
+##########################
 #gcsi_ciri_matrix <- gcsi_ciri_matrix + 1 #DeSeq calcualtes geometric mean, which is an issue with counts that have 0. +1 will not affect results, as log is taken anyway
-#gcsi_ciri_matrix <- log(gcsi_ciri_matrix + 1)
 colData <- data.frame(matrix(nrow=ncol(gcsi_ciri_matrix), ncol=2))
 colnames(colData) <- c("type", "dataset")
 colData$type <- "cell-line"
@@ -84,23 +103,113 @@ dds_f <- DESeq(dds2)
 gcsi_counts_normalized <-counts(dds_f, normalized = TRUE) 
 
 
+##########################
+#########CIRI2 CI#########
+##########################
 
-####calculate condordance index####
 
-colnames(gcsi_counts_normalized) <- gsub("gcsi","", colnames(gcsi_counts_normalized))
-colnames(gcsi_counts_normalized) <- gcsi$cellid[match(colnames(gcsi_counts_normalized), rownames(gcsi))]
+colnames(gcsi_ciri_matrix_norm) <- gsub("gcsi","", colnames(gcsi_ciri_matrix_norm))
+colnames(gcsi_ciri_matrix_norm) <- gcsi$cellid[match(colnames(gcsi_ciri_matrix_norm), rownames(gcsi))]
+
+
+
 
 gCSI <- readRDS("/Users/anthmam/Desktop/Projects/BHKLAB/PSets/gCSI.rds")
+CCLE <- readRDS("/Users/anthmam/Desktop/Projects/BHKLAB/PSets/CCLE.rds")
+CTRPv2 <- readRDS("/Users/anthmam/Desktop/Projects/BHKLAB/PSets/CTRPv2.rds")
+GDSC2 <- readRDS("/Users/anthmam/Desktop/Projects/BHKLAB/PSets/GDSC2.rds")
+
+combined_mean <- as.data.frame(rowMeans(gcsi_ciri_matrix_norm[, c("SR", "SR")], na.rm = TRUE))
+idx <- which(duplicated(colnames(gcsi_ciri_matrix_norm)))
+gcsi_ciri_matrix_norm <- gcsi_ciri_matrix_norm[,-idx]
+v <- combined_mean$`rowMeans(gcsi_ciri_matrix_norm[, c("SR", "SR")], na.rm = TRUE)`
+gcsi_ciri_matrix_norm$SR <- v
+  
+
 
 sensitivity_drug <- summarizeSensitivityProfiles(pSet = gCSI, sensitivity.measure = "aac_recomputed", fill.missing = F)
+#missingSamples <- colnames(gcsi_ciri_matrix_norm)[which(!colnames(gcsi_ciri_matrix_norm) %in% colnames(sensitivity_drug))]
 
-commonSamples <- intersect(colnames(sensitivity_drug),colnames(gcsi_counts_normalized))
+sensitivity_drug_ctrpv2 <- as.data.frame(summarizeSensitivityProfiles(pSet = CTRPv2, sensitivity.measure = "aac_recomputed", cell.lines = colnames(gcsi_ciri_matrix_norm), fill.missing = F))
+sensitivity_drug_ccle <- as.data.frame(summarizeSensitivityProfiles(pSet = CCLE, sensitivity.measure = "aac_recomputed", cell.lines = colnames(gcsi_ciri_matrix_norm), fill.missing = F))
+sensitivity_drug_gdsc <- as.data.frame(summarizeSensitivityProfiles(pSet = GDSC2, sensitivity.measure = "aac_recomputed", cell.lines = colnames(gcsi_ciri_matrix_norm), fill.missing = F))
 
-ci <- survcomp::concordance.index(sensitivity_drug["Bortezomib",commonSamples], surv.time = unlist(-gcsi_counts_normalized["ENSG00000078808.17_5", commonSamples]), surv.event = rep(1,length(sensitivity_drug[commonSamples])),outx = F, method="noether")
+#aa <- rbind.fill(sensitivity_drug, sensitivity_drug_ctrpv2, sensitivity_drug_gdsc,sensitivity_drug_ccle)
+#aa <- rbind(as.matrix(sensitivity_drug), as.matrix(sensitivity_drug_ctrpv2), as.matrix(sensitivity_drug_gdsc),as.matrix(sensitivity_drug_ccle))
+
+#sensitivity_drug$row.names <- rownames(sensitivity_drug)
+#sensitivity_drug_ctrpv2$row.names <- rownames(sensitivity_drug_ctrpv2)
+#sensitivity_drug_gdsc$row.names <- rownames(sensitivity_drug_gdsc)
+#sensitivity_drug_ccle$row.names <- rownames(sensitivity_drug_ccle)
+
+
+#ci <- survcomp::concordance.index(sensitivity_drug["Bortezomib",commonSamples], surv.time = unlist(-gcsi_ciri_matrix_norm["ENSG00000078808.17_5", commonSamples]), surv.event = rep(1,length(sensitivity_drug[commonSamples])),outx = F, method="noether")
+
+commonSamples <- intersect(colnames(sensitivity_drug),colnames(gcsi_ciri_matrix_norm))
+
+computeCI <- function(circRNA_normalized_data, sensitivity_data, samples){
+  
+  genes <- rownames(circRNA_normalized_data)
+  drugs <- unique(rownames(sensitivity_data))
+  commonSamples <- samples
+  combinations <- as.data.frame(expand.grid.unique(genes, drugs, include.equals = TRUE))
+  combinations$ci <- NA
+  combinations$pvalue <- NA
+  colnames(combinations) <- c("gene","drug","ci","pvalue")
+  
+  for (i in 1:nrow(combinations)){
+    print(paste0(i, " out of ", nrow(combinations), "complete"))
+    tt <- sensitivity_drug[combinations[,2][i],commonSamples]
+    tt[which(is.na(tt))] <- 0 #some sensitivities are NA due to filterNoisyCurve function, which causes error when running CI with survcomp
+    ci <- survcomp::concordance.index(tt, surv.time = unlist(-gcsi_ciri_matrix_norm[combinations[,1][i], commonSamples]), surv.event = rep(1,length(sensitivity_drug[commonSamples])),outx = F, method="noether")
+    combinations$pvalue[i] <- ci$p.value
+    combinations$ci[i] <- ci$c.index
+  }
+  
+  return(combinations)
+}
+
+gCSI_sens <- computeCI(circRNA_normalized_data=gcsi_ciri_matrix_norm, sensitivity_data=sensitivity_drug, samples = commonSamples)
+
+gCSI_sens_filtered <- gCSI_sens[which(gCSI_sens$pvalue < 0.05 & gCSI_sens$ci > 0.5),]
 
 
 
-rnaSeq_gCSI <- t(exprs(summarizeMolecularProfiles(gCSI,mDataType = "Kallisto_0.46.1.rnaseq",fill.missing = F)))
+
+
+
+
+
+
+
+
+
+
+
+
+ci <- survcomp::concordance.index(tt, surv.time = unlist(-gcsi_ciri_matrix_norm["ENSG00000237973.1_4", commonSamples]), surv.event = rep(1,length(sensitivity_drug[commonSamples])),outx = F, method="noether")
+
+commonSamples <- intersect(colnames(sensitivity_drug),colnames(gcsi_ciri_matrix_norm))
+genes <- rownames(gcsi_ciri_matrix_norm)
+drugs <- unique(rownames(sensitivity_drug))
+combinations <- as.data.frame(expand.grid.unique(genes, drugs, include.equals = TRUE))
+combinations$ci <- NA
+combinations$pvalue <- NA
+colnames(combinations) <- c("gene","drug","ci","pvalue")
+for (i in 1:nrow(combinations)){
+  tt <- sensitivity_drug[combinations[,2][i],commonSamples]
+  tt[which(is.na(tt))] <- 0 #some are NA due to filterNoisyCurve function, which causes error when running CI with survcomp
+  ci <- survcomp::concordance.index(tt, surv.time = unlist(-gcsi_ciri_matrix_norm[combinations[,1][i], commonSamples]), surv.event = rep(1,length(sensitivity_drug[commonSamples])),outx = F, method="noether")
+  combinations$pvalue[i] <- ci$p.value
+  combinations$ci[i] <- ci$c.index
+}
+
+combinations_fil <- combinations[which(combinations$pvalue < 0.05 & combinations$ci > 0.5),]
+
+
+
+
+rnaSeq_gCSI <- t(exprs(summarizeMolecularProfiles(gCSI,mDataType = "Kallisto_0.46.1.rnaseq", fill.missing = F)))
 
 
 
@@ -114,13 +223,49 @@ rnaSeq_gCSI <- t(exprs(summarizeMolecularProfiles(gCSI,mDataType = "Kallisto_0.4
 #   rate / sum(rate) * 1e6
 # }
 
-#library(scales)
 
-#rpm <- function(counts) {
-  #rtrn <- apply(counts, 2, function(x) {
-    #sc <- sum(x, na.rm=T) / 1000000
-    #x / sc
-  #})
-  #rtrn[is.na(rtrn)] <- 0
-  #rtrn
-#}
+
+
+
+
+
+summarizeCIRCMatrix <- function(dir_path){
+  
+  circ_files <- list.files(path=dir_path,  pattern = ".txt", full.names = T, recursive = TRUE)
+  gene_matrix <- data.frame(matrix(ncol=length(circ_files), nrow = length(rownames(features_gene))))
+  rownames(gene_matrix) <- rownames(features_gene)
+  
+  for (f in 1:length(circ_files)) {
+    sample <- read.table(file = circ_files[f], 
+                         sep = '\t', 
+                         header = FALSE)
+    
+    colnames(sample) <- c("chrom","start","end", "name","score","strand","thickStart","thickEnd","itemRgb","exonCount","exonSizes", "exonOffsets", "readNumber", "circType", "geneName", "isoformName", "index", "flankIntron")
+    sample_name <- sub(".*/ *(.*?) *.txt.*", "\\1", circ_files[f])
+    sample_name <- gsub("_circularRNA_known", "", sample_name)
+    sample <- sample[which(sample$circType=="circRNA"),]
+    sample <- sample[which(sample$readNumber >= 2),]
+    
+    #get gene-id from gencode v33lift37 feature_transcript data-frame, as circexplorer2 does not provide it in result files
+    
+    sample$gene_id <- features_transcript$gene_id[match(sample$isoformName, features_transcript$transcript_id)]
+    
+    #group sum of read number per gene_id
+    gene_reads <- sample %>% 
+      group_by(gene_id) %>% 
+      summarise(readNumber = sum(readNumber))
+    
+    gene_reads <- as.data.frame(gene_reads)
+    
+    rownames(gene_reads) <- gene_reads$gene_id
+    gene_matrix[rownames(gene_reads), f] <- gene_reads$readNumber
+    names(gene_matrix)[f] <- sample_name
+    
+  }
+  gene_matrix <- gene_matrix[rowSums(is.na(gene_matrix)) != ncol(gene_matrix), ]#remove rows that have NO exp (NA) for any sample
+  gene_matrix[is.na(gene_matrix)] <- 0
+  return(gene_matrix)
+} 
+
+
+gcsi_test <- summarizeCIRCMatrix(dir_path = "/Users/anthmam/Desktop/Projects/BHKLAB/ncRNA/results/CIRCexplorer2/gCSI/unmapped_method/annotate")
